49830473e08e575e41f802664c6e923fd43d0231
Luan Nascimento <luanoliveira1107@gmail.com>
2021-12-03 19:54:37 -0300

    Tela de cadastro da ong

---
+++ src/components/layouts/header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Menu from "../layouts/menu"
+import '../../assets/css/style.css'
+
+
+const Header = () =>{
+  return(
+    <header className="app-header">
+      <span className="app-header__logo"/>
+      <Menu />
+    </header>
+  )
+}
+
+export default Header
