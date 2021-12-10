3b1772c9cf5cb928333c77b99d91a4f8b69781d1
Mateus Soares <mateussoaress1997@gmail.com>
2021-12-10 18:02:18 -0300

    Add more logic to list ongs page

--- src/components/ongs/Ongs.jsx
+++ src/components/ongs/Ongs.jsx
@@ -5,11 +5,47 @@ import { Main } from './styled';
 import Filters from "./Filters";
 import OngList from './OngList';
 import Footer from '../layouts/footer';
+import i1 from '../../assets/img/ong1.png';
+import i2 from '../../assets/img/ong2.png';
+import i3 from '../../assets/img/ong3.png';
+
+const ongsArr = [
+    {
+        name: "Ong caminho das crianças",
+        img: i1,
+        category: "Crianças e adolescentes"
+    },
+    {
+        name: "Juntos contra os vicios",
+        img: i2,
+        category: "Alcoolismo, drogas"
+    },
+    {
+        name: "Amor de mãe",
+        img: i3,
+        category: "Mulheres adultas"
+    },
+    {
+        name: "1",
+        img: i1,
+        category: "Crianças e adolescentes"
+    },
+    {
+        name: "2",
+        img: i2,
+        category: "Alcoolismo, drogas"
+    },
+    {
+        name: "3",
+        img: i3,
+        category: "Mulheres adultas"
+    }
+]
 
 function Ongs() {
     const [ categories, setCategories ] = useState([]);
     const [ searchName, setSearchName ] = useState("");
-    const [ ongs, setOngs ] = useState([]);
+    const [ ongs, setOngs ] = useState(ongsArr);
     const [ page, setPage ] = useState(0);
     const [ hasPrevious, setHasPrevious ] = useState(false);
     const [ hasNext, setHasNext ] = useState(true);
