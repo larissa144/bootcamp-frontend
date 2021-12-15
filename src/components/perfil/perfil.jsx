9b661967439f3bf5cb1a1ae565848f82dcf61712
Kleber Tomazi <kleberluiz9@gmail.com>
2021-12-14 22:07:17 -0300

    Consumindo a API para obtenção dos dados e população do componente de Perfil

--- src/components/perfil/perfil.jsx
+++ src/components/perfil/perfil.jsx
@@ -12,57 +12,57 @@ import { Main, PerfilContent, PerfilSocialMedias, Divisor } from './styled'
 function Perfil() {
   const ongID = "192c3ac0-0ea9-485f-8ecf-ed1a1330639b";
   const { keycloak, initialized } = useKeycloak();
-  const [ ongData, setOngData ] = useState({})
+  const [ongData, setOngData] = useState({})
 
-    useEffect(async () => {
-        if(initialized) {
-            const result = await axios.get(`http://ec2-3-17-26-83.us-east-2.compute.amazonaws.com:8080/ongs/${ongID}`, {
-                headers: {
-                    Authorization: "Bearer " + keycloak.token
-                }
-            });
-
-            setOngData(result.data);
+  useEffect(async () => {
+    if (initialized) {
+      const result = await axios.get(`http://ec2-3-17-26-83.us-east-2.compute.amazonaws.com:8080/ongs/${ongID}`, {
+        headers: {
+          Authorization: "Bearer " + keycloak.token
         }
-    }, [ initialized ]);
+      });
+      console.log(result.data);
+      setOngData(result.data);
+    }
+  }, [initialized]);
 
-return (
-  <>
-    <Header />
-    <Main>
-      <PerfilContent>
+  return (
+    <>
+      <Header />
+      <Main>
+        <PerfilContent>
 
-        <PerfilOng
-          name={ongData.nome}
-          img={i1}
-          tel={"(11) 99999-9999"}
-          email={"email@email.com"}
-          address={"Rua das Crianças, 123"}
-          category={"Crianças e Adolescentes"}
-        />
+          <PerfilOng
+            name={ongData.nome ? ongData.nome : "Nome da ONG"}
+            img={i1}
+            tel={ongData.contato.telefone ? ongData.contato.telefone : "Telefone da ONG"}
+            email={ongData.contato.email ? ongData.contato.email : "Email da ONG"}
+            address={ongData.contato.endereco ? ongData.contato.endereco : "Endereço da ONG"}
+            category={"Crianças e Adolescentes"}
+          />
 
-      </PerfilContent>
-      <Divisor></Divisor>
+        </PerfilContent>
+        <Divisor></Divisor>
 
-      <PerfilSocialMedias>
+        <PerfilSocialMedias>
 
-        <MidiaSocial
-          img={i1}
-          user={"@instagram"}
-        />
-        <MidiaSocial
-          img={i1}
-          user={"@instagram"}
-        />
-        <MidiaSocial
-          img={i1}
-          user={"@instagram"}
-        />
+          <MidiaSocial
+            img={i1}
+            user={"@instagram"}
+          />
+          <MidiaSocial
+            img={i1}
+            user={"@instagram"}
+          />
+          <MidiaSocial
+            img={i1}
+            user={"@instagram"}
+          />
 
-      </PerfilSocialMedias>
-    </Main>
-  </>
-);
+        </PerfilSocialMedias>
+      </Main>
+    </>
+  );
 }
 
 export default Perfil
