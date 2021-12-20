031dd16ac087e29c6904129ee242af77a8dd92c9
vinicius-schulz <vinicius.schulz.cntr@spassu.com.br>
2021-12-20 09:12:54 -0300

    ajuste

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -6,22 +6,22 @@ import Cadastro from '../cadastro/cadastro';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import LandingPage from '../../pages/landing-page';
 import Perfil from "../perfil/perfil"
-import { ReactKeycloakProvider } from '@react-keycloak/web'
 import keycloak from '../../utils/keycloak'
+import { ReactKeycloakProvider } from '@react-keycloak/web'
 import PrivateRoute from '../layouts/private-route';
 
 function App() {
     return (
-        <ReactKeycloakProvider authClient={keycloak} initOptions={{checkLoginIframe: false}} >
+        <ReactKeycloakProvider authClient={keycloak} initOptions={{ checkLoginIframe: false }} >
             <BrowserRouter>
                 <Routes>
-                    <Route path="/" element={<LandingPage />}/>
-                    <Route path="/login" element={<Secured />}/>
-                    <Route path="/feed" element={<PrivateRoute children={<Ongs/>} />}/>
-                    <Route path="/ongs" element={<PrivateRoute children={<Ongs/>} />}/>
-                    <Route path="/ongs/:id" element={<PrivateRoute children={<Perfil/>} />}/>
-                    <Route path="/criarNovaOng" element={<PrivateRoute children={<CriarNovaOng/>} />}/>
-                    <Route path="/cadastro" element={<Cadastro/>}/>
+                    <Route path="/" element={<LandingPage />} />
+                    <Route path="/login" element={<Secured />} />
+                    <Route path="/feed" element={<PrivateRoute children={<Ongs />} />} />
+                    <Route path="/ongs" element={<PrivateRoute children={<Ongs />} />} />
+                    <Route path="/ongs/:id" element={<PrivateRoute children={<Perfil />} />} />
+                    <Route path="/criarNovaOng" element={<PrivateRoute children={<CriarNovaOng />} />} />
+                    <Route path="/cadastro" element={<Cadastro />} />
                 </Routes>
             </BrowserRouter>
         </ReactKeycloakProvider>
--- src/utils/keycloak.js
+++ src/utils/keycloak.js
@@ -1,19 +1,8 @@
 import Keycloak from 'keycloak-js';
-
-
-const keycloakConfig = {
-    "realm": "idonation",
-    "auth-server-url": "http://3.141.196.157:9090/auth",
-    "ssl-required": "none",
-    "resource": "idonation-web",
-    "public-client": true,
-    "verify-token-audience": true,
-    "use-resource-role-mappings": true,
-    "confidential-port": 0
-}
+import jsonObhect from './keycloak.json';
 
 // Setup Keycloak instance as needed
 // Pass initialization options as required or leave blank to load from 'keycloak.json'
-const keycloak = new Keycloak(keycloakConfig);
+const keycloak = new Keycloak(jsonObhect);
 
 export default keycloak;
