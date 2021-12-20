bbd289392f0ed48b29d0caa9ad6950b349e47831
vinicius-schulz <vinicius.schulz.cntr@spassu.com.br>
2021-12-19 23:18:35 -0300

    ajuste

--- src/utils/keycloak.js
+++ src/utils/keycloak.js
@@ -2,6 +2,15 @@ import Keycloak from 'keycloak-js';
 
 // Setup Keycloak instance as needed
 // Pass initialization options as required or leave blank to load from 'keycloak.json'
-const keycloak = new Keycloak("https://raw.githubusercontent.com/larissa144/bootcamp-frontend/main/public/keycloak.json");
+const keycloak = new Keycloak({
+    "realm": "idonation",
+    "auth-server-url": "http://3.141.196.157:9090/auth",
+    "ssl-required": "none",
+    "resource": "idonation-web",
+    "public-client": true,
+    "verify-token-audience": true,
+    "use-resource-role-mappings": true,
+    "confidential-port": 0
+});
 
 export default keycloak;
