ba3340b3bcdd0201caf7a99b09b46769407aec78
Mateus Soares <mateussoaress1997@gmail.com>
2021-12-29 14:13:43 -0300

    Change identation

--- src/components/ong/criarNovaOng.jsx
+++ src/components/ong/criarNovaOng.jsx
@@ -12,40 +12,39 @@ import { FiPhone } from "react-icons/fi";
 import { FiMapPin } from "react-icons/fi";
 import { FiFileText } from "react-icons/fi";
 
-
 const DivImg = styled.div`
-  display: flex;
-  width: 100%;
-  justify-content: center;
-  align-items: center;
+    display: flex;
+    width: 100%;
+    justify-content: center;
+    align-items: center;
 `
 const DivCard = styled.div`
-  display: flex;
-  width: 100%;
-  justify-content: center;
-  align-items: center;
+    display: flex;
+    width: 100%;
+    justify-content: center;
+    align-items: center;
 `
 const DivTitles = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  width: 100%;
-  height: 20%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: 20%;
 `
 const DivInput = styled.div`
-  margin-bottom: auto;
-  flex-direction: column;
-  display: flex;
-  width: 100%;
+    margin-bottom: auto;
+    flex-direction: column;
+    display: flex;
+    width: 100%;
 `  
 const DivButton = styled.div`
-  margin-bottom: auto;
-  justify-content: center;
-  align-items: center;
-  display: flex;
-  width: 100%;
-  height: 20%;
+    margin-bottom: auto;
+    justify-content: center;
+    align-items: center;
+    display: flex;
+    width: 100%;
+    height: 20%;
 `
 const Title = styled.p`
     color: #5A5657;
@@ -56,75 +55,73 @@ const Title = styled.p`
 `
 
 const Subtitle = styled.p`
-  color: #5A5657;
-  font-size: 15px;
-  margin-top: 2px;
+    color: #5A5657;
+    font-size: 15px;
+    margin-top: 2px;
 `
 
-
 function CriarNovaOng() {
-
     return(
-      <>
-        <Header />
-          <ColorBodyWhite>
-              <DivImg>
-                  <h3>Upload de imagem</h3>
-              </DivImg>
-              
-              <DivCard>
-                   <Card width={'50%'}> 
-                      <DivTitles>
-                          <Title>Criar uma nova ONG</Title>
-                          <Subtitle>É muito simples e fácil.</Subtitle>
-                      </DivTitles>
-                     
-                      <DivInput>
-                          <Input
-                              // onChange={handleChangeEmail}
-                              icon={FiUser}
-                              textInput='Nome da ONG'
-                              width={'85%'} 
-                              height={'8%'}
-                          />
+        <>
+            <Header />
+            <ColorBodyWhite>
+                <DivImg>
+                    <h3>Upload de imagem</h3>
+                </DivImg>
+
+                <DivCard>
+                    <Card width={'50%'}> 
+                        <DivTitles>
+                            <Title>Criar uma nova ONG</Title>
+                            <Subtitle>É muito simples e fácil.</Subtitle>
+                        </DivTitles>
+
+                        <DivInput>
                             <Input
-                              // onChange={handleChangeEmail}
-                              icon={FiMail}
-                              textInput='E-mail'
-                              width={'85%'} 
-                              height={'8%'}
-                          />
+                            // onChange={handleChangeEmail}
+                            icon={FiUser}
+                            textInput='Nome da ONG'
+                            width={'85%'} 
+                            height={'8%'}
+                            />
                             <Input
-                              // onChange={handleChangeEmail}
-                              icon={FiPhone}
-                              textInput='Telefone'
-                              width={'85%'} 
-                              height={'8%'}
-                          />
+                            // onChange={handleChangeEmail}
+                            icon={FiMail}
+                            textInput='E-mail'
+                            width={'85%'} 
+                            height={'8%'}
+                            />
                             <Input
-                              // onChange={handleChangeEmail}
-                              icon={FiMapPin}
-                              textInput='Endereço'
-                              width={'85%'} 
-                              height={'8%'}
-                          />
+                            // onChange={handleChangeEmail}
+                            icon={FiPhone}
+                            textInput='Telefone'
+                            width={'85%'} 
+                            height={'8%'}
+                            />
                             <Input
-                              // onChange={handleChangeEmail}
-                              icon={FiFileText}
-                              textInput='Categoria'
-                              width={'85%'} 
-                              height={'8%'}
-                          />
-                      </DivInput>
-                      <DivButton>
-                          <Button 
-                              textButton='Criar nova conta' 
-                              width={'45%'} 
-                              height={'25%'}
-                          />
-                      </DivButton>
-                  </Card>
-              </DivCard>
+                            // onChange={handleChangeEmail}
+                            icon={FiMapPin}
+                            textInput='Endereço'
+                            width={'85%'} 
+                            height={'8%'}
+                            />
+                            <Input
+                            // onChange={handleChangeEmail}
+                            icon={FiFileText}
+                            textInput='Categoria'
+                            width={'85%'} 
+                            height={'8%'}
+                            />
+                        </DivInput>
+                        <DivButton>
+                            <Button 
+                            textButton='Criar nova conta' 
+                            width={'45%'} 
+                            height={'25%'}
+                            />
+                        </DivButton>
+                    </Card>
+                </DivCard>
             </ColorBodyWhite>
         </>
     )
