e6fd652626f43169ba3dcb92e567d644cc148ea5
Mateus Soares <mateussoaress1997@gmail.com>
2021-12-31 18:43:06 -0300

    Add social media registration for ongs

--- src/components/cadastro-ong/cadastro-ong.jsx
+++ src/components/cadastro-ong/cadastro-ong.jsx
@@ -35,9 +35,16 @@ function CadastroOng() {
     const [ imgPreview, setImgPreview ] = useState(img)
     const [ categories, setCategories ] = useState([])
     const [ message, setMessage ] = useState("");
+    const [ socialMediaList, setSocialMediaList ] = useState([]);
+    const [ socialMediaData, setSocialMediaData ] = useState({});
+
     const { keycloak, initialized } = useKeycloak();
     
     const inputRef = React.useRef(null);
+
+    useEffect(() => {
+        console.log({socialMediaData})
+    }, [socialMediaData])
 
     useEffect(() => {
         if(!ongImg) return;
@@ -61,6 +68,32 @@ function CadastroOng() {
         }
         getCategories();
     }, [ initialized, keycloak.token ]);
+
+    useEffect(() => {
+        const getSocialMediaList = async () => {
+            if(initialized) {
+                const result = await axios.get("http://ec2-3-17-26-83.us-east-2.compute.amazonaws.com:8080/redes-sociais", {
+                    headers: {
+                        Authorization: "Bearer " + keycloak.token
+                    },
+                });
+                console.log("***", {
+                    result
+                })
+                setSocialMediaList(result.data.content);
+            }
+        }
+        getSocialMediaList();
+    }, [ initialized, keycloak.token, setSocialMediaList ]);
+
+    const handleSocialMediaChange = (socialMediaId) => {
+        return (e) => {
+            setSocialMediaData({
+                ...socialMediaData,
+                [socialMediaId]: e.target.value
+            })
+        }
+    }
 
     const createOng = async () => {
         const config = {
@@ -98,6 +131,21 @@ function CadastroOng() {
             );
         }
     };
+    
+    const addOngSocialMedias = async (ongId) => {
+        for (const socialMediaKey of Object.keys(socialMediaData)) {
+            const socialMedia = socialMediaData[socialMediaKey];
+            const config = {
+                headers: {
+                    Authorization: "Bearer " + keycloak.token
+                }
+            };
+            const payload = {
+                identificador: socialMedia,
+            };
+            await axios.post(`http://ec2-3-17-26-83.us-east-2.compute.amazonaws.com:8080/ongs/${ongId}/redes-sociais/${socialMediaKey}`, payload, config);
+        }
+    };
 
     const registerOng = async () => {
         try {
@@ -105,7 +153,7 @@ function CadastroOng() {
             
             const result = await createOng();
             await addOngImage(result.data.id);
-            
+            await addOngSocialMedias(result.data.id);
             setMessage("Ong criada!");
             setTimeout(() => {
                 navigate(`/ongs/${result.data.id}`)
@@ -191,15 +239,21 @@ function CadastroOng() {
                                 <option value="" disabled>Selecione uma categoria</option>
                                 { categories.map(item => (<option key={item.nome} value={item.id}>{item.nome}</option>)) }
                             </Select>
-                            {/* <SubtitleSocialMedia>Redes Sociais</SubtitleSocialMedia>
-                            <Input
-                                value={address}
-                                onChange={handleChange(setAddress)}
-                                icon={FiMapPin}
-                                textInput='Endereço'
-                                width={'85%'} 
-                                height={'8%'}
-                            /> */}
+                            <SubtitleSocialMedia>Redes Sociais</SubtitleSocialMedia>
+                            {
+                                socialMediaList.map(socialMedia => {
+                                    return (
+                                        <Input
+                                            key={socialMedia.id}
+                                            value={socialMediaData[socialMedia.id] || ""}
+                                            onChange={handleSocialMediaChange(socialMedia.id)}
+                                            textInput={socialMedia.nome}
+                                            width={'85%'} 
+                                            height={'8%'}
+                                        />
+                                    )
+                                })
+                            }
                         </DivInput>
                         <DivButton>
                             <Button 
--- src/components/layouts/background-color.jsx
+++ src/components/layouts/background-color.jsx
@@ -2,7 +2,7 @@ import styled from 'styled-components'
 
 export const ColorBody = styled.div`
   width: 100vw;
-  height: 100vh;
+  min-height: 100vh;
   background: linear-gradient(121.03deg, #440A67 38.95%, #6A1B7C 75.35%, #89298C 88.6%);
   display: flex;
   flex-direction: row;
@@ -12,7 +12,7 @@ export const ColorBody = styled.div`
 
 export const ColorBodyWhite  = styled.div`
   width: 100vw;
-  height: 100vh;
+  min-height: 100vh;
   display: flex;
   flex-direction: row;
   justify-content: center;
