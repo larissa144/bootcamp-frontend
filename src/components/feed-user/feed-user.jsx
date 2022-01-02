e145c8ecf0ad984e506cf548affe66eded90bd83
larissa alves <larialvees123@gmail.com>
2022-01-02 13:27:20 -0300

    wip: layout textArea

--- src/components/feed-user/feed-user.jsx
+++ src/components/feed-user/feed-user.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import Header from '../layouts/header'
+import TextAreaComponent from '../layouts/text-area'
 
 const FeedUser = () => {
 
     return(
+        <>
         <Header />
+        </>
     )
 }
 
